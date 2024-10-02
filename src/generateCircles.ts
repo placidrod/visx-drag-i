57cf99529a28aa478ad3df7c43b5f713bd801a26
Placid Rodrigues <placidrod@gmail.com>
2024-10-03 12:18:05 +1300

    start

---
+++ src/generateCircles.ts
@@ -0,0 +1,32 @@
+import { getSeededRandom } from "@visx/mock-data";
+
+export interface Circle {
+  id: string;
+  radius: number;
+  x: number;
+  y: number;
+}
+
+const generateCircles = ({
+  width,
+  height,
+}: {
+  width: number;
+  height: number;
+}) => {
+  const radiusRandom = getSeededRandom(0.8);
+  const xRandom = getSeededRandom(0.5);
+  const yRandom = getSeededRandom(0.6);
+
+  return new Array(width < 360 ? 10 : 20).fill(1).map((d, i) => {
+    const radius = 50 - radiusRandom() * 20;
+    return {
+      id: `${i}`,
+      radius,
+      x: Math.round(xRandom() * (width - radius * 2) + radius),
+      y: Math.round(yRandom() * (height - radius * 2) + radius),
+    };
+  });
+};
+
+export default generateCircles;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import ParentSize from '@visx/responsive/lib/components/ParentSize';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import Example from './Example';
+import './sandbox-styles.css';
+
+const root = createRoot(document.getElementById('root')!);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ParentSize>
+    {({ width, height }) => <Example width={width} height={400} />}
+  </ParentSize>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
